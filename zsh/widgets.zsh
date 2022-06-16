0e37a8472515200056a0f528a026a5d414af8314
d3adb5 <me@d3adb5.net>
2022-06-16 00:18:46 -0300

    feat: jump to next tmpl field with C-g
    Add a ZLE widget that allows one to jump to the next "template" field
    with the keybind Ctrl + G. Fields are identified by text enclosed in
    double curly braces ({{ like this }}), and do not have any special
    syntax inside of them.

--- zsh/widgets.zsh
+++ zsh/widgets.zsh
@@ -12,5 +12,16 @@ fzf-smarky-pick-command() {
   zle reset-prompt
 }
 
+fzf-smarky-next-field() {
+  setopt localoptions noglobsubst noposixbuiltins pipefail 2>/dev/null
+  [ -z "$RBUFFER" ] && CURSOR=0
+  local subtracted="${RBUFFER#*\{\{}" offset
+  offset=$(( ${#RBUFFER} - ${#subtracted} - 2 + $CURSOR ))
+  CURSOR=offset; RBUFFER="${RBUFFER#*\}\}}"
+}
+
 zle -N fzf-smarky-pick-command
 bindkey '^J' fzf-smarky-pick-command
+
+zle -N fzf-smarky-next-field
+bindkey '^G' fzf-smarky-next-field
